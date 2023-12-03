b36b06812838b1b509f9d56e11dcf979aa13de37
gcpearse <glen.c.pearse@gmail.com>
2023-12-03 04:03:07 +0000

    adds button to scroll to task input form - amends ID names

--- to-do-list/src/components/Footer.jsx
+++ to-do-list/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
     <footer>
-      <button className="footer-button" id="help-button">Help</button>
-      <button className="footer-button" id="back-to-top-button" onClick={() => {
+      <button className="footer-button" id="scroll-down-button" onClick={() => {
+        const form = document.getElementById("task-input-form");
+        form.scrollIntoView({ behavior: "smooth" });
+      }}>Add tasks</button>
+      <button className="footer-button" id="scroll-up-button" onClick={() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
       }}>Back to top</button>
     </footer>
