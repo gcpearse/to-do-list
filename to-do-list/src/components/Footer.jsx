58b175bf252821279427f09fe2c4c526cf0fb76e
gcpearse <glen.c.pearse@gmail.com>
2024-03-03 22:49:56 +0000

    tidies up component

--- to-do-list/src/components/Footer.jsx
+++ to-do-list/src/components/Footer.jsx
@@ -1,13 +1,23 @@
 const Footer = () => {
   return (
     <footer>
-      <button className="footer-button" id="scroll-down-button" onClick={() => {
-        const form = document.getElementById("task-input-form");
-        form.scrollIntoView({ behavior: "smooth" });
-      }}>Add tasks</button>
-      <button className="footer-button" id="scroll-up-button" onClick={() => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }}>Back to top</button>
+      <button
+        className="footer-button"
+        id="scroll-down-button"
+        onClick={() => {
+          const form = document.getElementById("task-input-form");
+          form.scrollIntoView({ behavior: "smooth" });
+        }}>
+        Add tasks
+      </button>
+      <button
+        className="footer-button"
+        id="scroll-up-button"
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}>
+        Back to top
+      </button>
     </footer>
   );
 };
