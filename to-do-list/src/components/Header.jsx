29bec671881bb279bc90b11d842e7df944841aeb
gcpearse <glen.c.pearse@gmail.com>
2024-03-03 22:02:24 +0000

    removes specifies IDs from elements

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <div id="hero">
-      <h1 className="hero-text" id="hero-header">eTask</h1>
-      <h2 className="hero-text" id="hero-message">Your online to-do list</h2>
-    </div>
+    <header>
+      <h1>eTask</h1>
+      <h2>Your online to-do list</h2>
+    </header>
   );
 };
 
